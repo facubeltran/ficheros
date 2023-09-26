f37ba3b56ce13076734144a0b2ad30ff1d5f94d8
facubeltran <facundocristianbeltran@gmail.com>
2023-09-25 23:49:41 -0300

    3ra parte

--- Program.cs
+++ Program.cs
@@ -45,34 +45,52 @@
 
             try
             {
+                
+                string Empresa = "BANCO S INTERNACIONAL";
+                string Cuenta = "2345532";
+                int cantidadEmpleados= 0;
+                double totalSueldo=0;
+
                 //Pass the filepath and filename to the StreamWriter Constructor
                 StreamWriter sw = new StreamWriter("C:\\Users\\facubeltran\\Desktop\\reposgit\\bdt\\vv\\Empleados.txt");
+                StreamWriter swSueldos = new StreamWriter("C:\\Users\\facubeltran\\Desktop\\reposgit\\bdt\\vv\\Sueldos.txt");
+                swSueldos.WriteLine($"{Empresa};{Cuenta}");
+                Console.WriteLine($"{Empresa};{Cuenta}");
                 for (int i = 0; i < 100; i++)
                 {
                     string legajo;
                     string nombre;
                     if (i < 10) 
                     {
-                        legajo = $"00{rnd.Next(9)}{i}";
+                        legajo = $"00{rand(9)}{i}";
                         nombre = $"Juan Ramirez0{i} ";
                     }
                     else
                     {
-                        legajo = $"0{rnd.Next(9)}{i}";
+                        legajo = $"0{rand(9)}{i}";
                         nombre = $"Juan Ramirez{i} ";
                     }
 
-                    
-                    string categoria = $"0{categorias[rnd.Next(4)].TipoCategoria}";
+                    Categoria cat = categorias[rand(4)];
+                    string categoriaT = $"0{cat.TipoCategoria}";
                     string cuenta = $"000{rand(9)}{rand(9)}{rand(9)}{rand(9)}";
                     //Write a line of text
-                    sw.WriteLine($"{legajo}{nombre}{categoria}{cuenta}");
-                    //Write a second line of text
-                    
+                    sw.WriteLine($"{legajo}{nombre}{categoriaT}{cuenta}");
+
+
+
+                    //DETALLE
+                    double sueldoEmpleado = cat.Sueldo;
+                    swSueldos.WriteLine($"{cuenta};{nombre};{sueldoEmpleado}");
+
+                    ++cantidadEmpleados;
+                    totalSueldo += sueldoEmpleado;
                     //Close the file     
                 }
-
+                double totalRedondeado = Math.Round(totalSueldo, 2);
+                swSueldos.WriteLine($"{cantidadEmpleados};{totalRedondeado}");
                 sw.Close();
+                swSueldos.Close();
             }
             catch (Exception e)
             {
@@ -82,7 +100,8 @@
             {
                 Console.WriteLine("Executing finally block.");
             }
-   
+            //******************************
+           
 
         }
 
