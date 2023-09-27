fd7efdc8bff29af813f61efef54bcbb7889e408f
facubeltran <facundocristianbeltran@gmail.com>
2023-09-27 01:21:52 -0300

    guardado en funciones

--- Program.cs
+++ Program.cs
@@ -6,7 +6,106 @@
         {
             Random rnd = new Random();
             List<Categoria> categorias = new List<Categoria>();
-            String line;
+            String line = "";
+            leerCategorias(categorias, line);
+            crearEmpleados(rnd, categorias);
+            crearSueldosFichero(categorias, line);
+        }
+
+        private static string crearSueldosFichero(List<Categoria> categorias, string line)
+        {
+            try
+            {
+
+                string Empresa = "BANCO S INTERNACIONAL";
+                string Cuenta = "2345532";
+                int cantidadEmpleados = 0;
+                double totalSueldo = 0;
+
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamReader srSueldos = new StreamReader("C:\\Users\\facubeltran\\Desktop\\reposgit\\bdt\\vv\\Empleados.txt");
+                StreamWriter swSueldos = new StreamWriter("C:\\Users\\facubeltran\\Desktop\\reposgit\\bdt\\vv\\Sueldos.txt");
+                swSueldos.WriteLine($"{Empresa};{Cuenta}");
+                line = srSueldos.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    int cuenta = int.Parse(line.Substring(27, 7));
+                    string nombre = line.Substring(4, 20);
+                    int catInt = int.Parse(line.Substring(25, 2));
+                    //write the line to console window
+                    double sueldo = categorias.Find(sue => sue.TipoCategoria == catInt).Sueldo;
+
+
+                    //DETALLE
+                    swSueldos.WriteLine($"{cuenta};{nombre};{sueldo}");
+                    ++cantidadEmpleados;
+                    totalSueldo += sueldo;
+                    line = srSueldos.ReadLine();
+
+
+                }
+
+                Categoria cat = categorias[rand(4)];
+                double totalRedondeado = Math.Round(totalSueldo, 2);
+                swSueldos.WriteLine($"{cantidadEmpleados};{totalRedondeado}");
+                srSueldos.Close();
+                swSueldos.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+            return line;
+        }
+
+        private static void crearEmpleados(Random rnd, List<Categoria> categorias)
+        {
+            try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("C:\\Users\\facubeltran\\Desktop\\reposgit\\bdt\\vv\\Empleados.txt");
+                for (int i = 0; i < 100; i++)
+                {
+                    string legajo;
+                    string nombre;
+                    if (i < 10)
+                    {
+                        legajo = $"00{rand(9)}{i}";
+                        nombre = $"Juan Ramirez Gauss0{i} ";
+                    }
+                    else
+                    {
+                        legajo = $"0{rand(9)}{i}";
+                        nombre = $"Juan Ramirez Gauss{i} ";
+                    }
+
+                    Categoria cat = categorias[rand(4)];
+                    string categoriaT = $"0{cat.TipoCategoria}";
+                    string cuenta = $"{rnd.Next(1, 9)}0{rand(9)}0{rand(9)}0{rand(9)}";
+                    //Write a line of text
+                    sw.WriteLine($"{legajo}{nombre}{categoriaT}{cuenta}");
+                    //Close the file     
+                }
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        private static string leerCategorias(List<Categoria> categorias, string line)
+        {
             try
             {
                 //Pass the file path and file name to the StreamReader constructor
@@ -16,16 +115,16 @@
                 //Continue to read until you reach end of file
                 while (line != null)
                 {
-                    int legajo= int.Parse(line.Substring(0,2));
-                    string nombre= line.Substring(2,15);
-                    double sueldo= double.Parse(line.Substring(17, 8));
+                    int legajo = int.Parse(line.Substring(0, 2));
+                    string nombre = line.Substring(2, 15);
+                    double sueldo = double.Parse(line.Substring(17, 8));
                     //write the line to console window                    
                     categorias.Add(new Categoria(legajo, nombre, sueldo));
                     line = sr.ReadLine();
                 }
                 //close the file
                 sr.Close();
-                
+
             }
             catch (Exception e)
             {
@@ -43,113 +142,7 @@
                 Console.WriteLine($"Sueldo:{cat.Sueldo}");
             }
 
-            try
-            {
-                
-                string Empresa = "BANCO S INTERNACIONAL";
-                string Cuenta = "2345532";
-                int cantidadEmpleados= 0;
-                double totalSueldo=0;
-
-                //Pass the filepath and filename to the StreamWriter Constructor
-                StreamWriter sw = new StreamWriter("C:\\Users\\facubeltran\\Desktop\\reposgit\\bdt\\vv\\Empleados.txt");
-                StreamWriter swSueldos = new StreamWriter("C:\\Users\\facubeltran\\Desktop\\reposgit\\bdt\\vv\\Sueldos.txt");
-                swSueldos.WriteLine($"{Empresa};{Cuenta}");
-                Console.WriteLine($"{Empresa};{Cuenta}");
-                for (int i = 0; i < 100; i++)
-                {
-                    string legajo;
-                    string nombre;
-                    if (i < 10) 
-                    {
-                        legajo = $"00{rand(9)}{i}";
-                        nombre = $"Juan Ramirez Gauss0{i} ";
-                    }
-                    else
-                    {
-                        legajo = $"0{rand(9)}{i}";
-                        nombre = $"Juan Ramirez Gauss{i} ";
-                    }
-
-                    Categoria cat = categorias[rand(4)];
-                    string categoriaT = $"0{cat.TipoCategoria}";
-                    string cuenta = $"{rnd.Next(1,9)}0{rand(9)}0{rand(9)}0{rand(9)}";
-                    //Write a line of text
-                    sw.WriteLine($"{legajo}{nombre}{categoriaT}{cuenta}");
-
-
-
-                    //DETALLE
-                    double sueldoEmpleado = cat.Sueldo;
-                    swSueldos.WriteLine($"{cuenta};{nombre};{sueldoEmpleado}");
-
-                    ++cantidadEmpleados;
-                    totalSueldo += sueldoEmpleado;
-                    //Close the file     
-                }
-                double totalRedondeado = Math.Round(totalSueldo, 2);
-                swSueldos.WriteLine($"{cantidadEmpleados};{totalRedondeado}");
-                sw.Close();
-                swSueldos.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Executing finally block.");
-            }
-
-            try
-            {
-
-                string Empresa = "BANCO S INTERNACIONAL";
-                string Cuenta = "2345532";
-                int cantidadEmpleados = 0;
-                double totalSueldo = 0;
-
-                //Pass the filepath and filename to the StreamWriter Constructor
-                StreamReader srSueldos = new StreamReader("C:\\Users\\facubeltran\\Desktop\\reposgit\\bdt\\vv\\Empleados.txt");
-                StreamWriter swSueldos = new StreamWriter("C:\\Users\\facubeltran\\Desktop\\reposgit\\bdt\\vv\\Sueldos.txt");
-                swSueldos.WriteLine($"{Empresa};{Cuenta}");
-                line = srSueldos.ReadLine();
-                //Continue to read until you reach end of file
-                while (line != null)
-                {
-                    int cuenta = int.Parse(line.Substring(27,7));
-                    string nombre = line.Substring(4, 20);
-                    int catInt = int.Parse(line.Substring(25, 2));
-                    //write the line to console window
-                    double sueldo = categorias.Find(sue => sue.TipoCategoria == catInt).Sueldo;
-                    
-                    
-                    //DETALLE
-                    swSueldos.WriteLine($"{cuenta};{nombre};{sueldo}");
-                    ++cantidadEmpleados;
-                    totalSueldo += sueldo;
-                    line = srSueldos.ReadLine();
-
-                    
-                }
-
-                Categoria cat = categorias[rand(4)];
-                double totalRedondeado = Math.Round(totalSueldo, 2);
-                swSueldos.WriteLine($"{cantidadEmpleados};{totalRedondeado}");
-                srSueldos.Close();
-                swSueldos.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Executing finally block.");
-            }
-            //******************************
-
-
+            return line;
         }
 
         private static int rand(int limite)
